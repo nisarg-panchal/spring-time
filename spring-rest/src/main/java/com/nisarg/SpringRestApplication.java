64464f6b338dbf3be8129866de2882bfdccd0cec
Nisarg Panchal <nisarg1184@gmail.com>
2021-10-05 00:25:04 +1000

    Added visual spring-boot-admin

--- spring-rest/src/main/java/com/nisarg/SpringRestApplication.java
+++ spring-rest/src/main/java/com/nisarg/SpringRestApplication.java
@@ -1,13 +1,14 @@
 package com.nisarg;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.web.reactive.config.EnableWebFlux;
 
 @EnableR2dbcRepositories
 @EnableR2dbcAuditing
+@EnableAdminServer
 @SpringBootApplication
 public class SpringRestApplication {
 
